7e0843edf8187e7cc9e8ac988cf5c2db63cc0111
Dibya2022 <kumardibya20@gmail.com>
2024-07-13 18:35:20 +0530

    Day_1

---
+++ Day_1.js
@@ -0,0 +1,40 @@
+var x = 5;
+console.log(x);
+// Output: 5
+let fruits = "Apple";
+console.log(fruits);
+// Output: Apple
+
+let theVal = true;
+console.log(theVal);
+// Output: true
+
+const data1 = 12;
+console.log(typeof data1);
+// Output: number
+
+const data2 = "Dibya";
+console.log(typeof data2);
+// Output: string
+
+const data3 = true;
+console.log(data3);
+// Output: boolean
+
+const data4 = { firstName: "Dibya" };
+console.log(typeof data4);
+// Output: object
+
+const data5 = ["Audi", "BMW", "Marcedees", "Ferari", "RolllesRoyes"];
+console.log(typeof data5);
+// Output: object
+
+let value = "Apple";
+let newValue = "Grapes";
+console.log(value, newValue);
+// Output: Apple Grapes
+
+const val1 = "Tree";
+// const val1 = "Parrot";
+console.log(val1);
+// Output: SyntaxError: Identifier 'val1' has already been declared
