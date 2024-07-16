34fcbf17342410bde56488debd4ca7763c8e28c8
Dibya2022 <kumardibya20@gmail.com>
2024-07-16 23:51:05 +0530

    Day_4

---
+++ Day_4.js
@@ -0,0 +1,35 @@
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+// Output: 1,2,3,4,5,6,7,8,9,10
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`5 * ${i} = ${5 * i}`);
+}
+// Output: 5 * 1 = 5, 5 * 2 = 10, 5 * 3 = 15, 5 * 4 = 20, 5 * 5 = 25, 5 * 6 = 30, 5 * 7 = 35, 5 * 8 = 40, 5 * 9 = 45, 5 * 10 = 50
+
+let sum = 0;
+let i = 1;
+while (i <= 10) {
+  sum += i;
+  i++;
+}
+console.log(`Sum of 1 to 10 is ${sum}`);
+// Output: Sum of 1 to 10 is 55
+
+let num = 0;
+do {
+  num++;
+  console.log(num);
+} while (num < 5);
+// Output: 1,2,3,4,5
+
+let number = 5;
+let factorial = 1;
+let j = 1;
+do {
+  factorial *= j;
+  j++;
+} while (j <= number);
+console.log(`factorila of ${number} is ${factorial}`);
+// Output: factorila of 5 is 120
